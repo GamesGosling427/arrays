a49a45442e3869c94cc515b76c29d77b39a85530
Rohan <111503422+GamesGosling427@users.noreply.github.com>
2022-08-20 12:05:49 +0530

    Add files via upload

---
+++ ArrayProgram.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class ArrayProgram {
+
+	public static void main(String[] args) {
+		int[]marks=new int[3];
+		marks[0]=97;
+		marks[1]=49;
+		marks[2]=99;
+		//System.out.println(marks[0]);
+		//System.out.println(marks[1]);
+		//System.out.println(marks[2]);
+		for(int i=0; i<3; i++)
+		{
+			System.out.println(marks[i]);
+		}
+	}
+
+}
---
+++ ArrayProgram2.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class ArrayProgram2 {
+
+	public static void main(String[] args) {
+		int marks[]= {43,43,54};
+		for(int i=0; i<3; i++)
+		{
+			System.out.println(marks[i]);
+		}
+		
+
+	}
+
+}
